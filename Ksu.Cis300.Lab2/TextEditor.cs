3ce399ebc485242f74b07e2931bb344ceaccf653
hschrick <hschrick@ksu.edu>
2017-01-20 11:45:01 -0600

    submition

---
+++ Ksu.Cis300.Lab2/TextEditor.cs
@@ -0,0 +1,44 @@
+﻿/*Ksu.Cis300.Lab2.cs
+ * Author: Harris Schrick
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ksu.Cis300.Lab2
+{
+    ///Text Editor form and components
+    public partial class TextEditor : Form
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+        }
+
+        //opens file selection from computer.
+        private void uxOpen_Click(object sender, EventArgs e)
+        {
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Can't open file " + OpenFile.FileName);
+            }
+        }
+
+        //saves and or overwrites file to one that already exists.
+        private void uxSaveAs_Click(object sender, EventArgs e)
+        {
+            if (SaveFile.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Can't write to file " + SaveFile.FileName);
+            }
+        }
+        
+    }
+}
